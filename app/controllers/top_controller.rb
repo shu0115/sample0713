2c4825732038bbdc7c8ae22d7f88beec05e8a2fe
shu0115 <raisondetre0115@gmail.com>
2012-07-13 05:33:01 +0900

    update

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -6,9 +6,12 @@ class TopController < ApplicationController
   #-------#
   def index
     Tweet.get_tweet( current_user ) unless session[:user_id].blank?
+    print "[ Tweet.count ] : " ; p Tweet.count ;
     @tweets = Tweet.all
+#    @tweets = (Tweet.count > 0) ? Tweet.all : nil
     print "[ Tweet.class.name ] : " ; p Tweet.class.name ;
     print "[ @tweets ] : " ; p @tweets ;
+    print "[ @tweets.class.name ] : " ; p @tweets.class.name ;
   end
 
 end
