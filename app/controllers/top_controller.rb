eace3023ec6dcfd52990bb4bd1cd2f065a42bc55
shu0115 <raisondetre0115@gmail.com>
2012-07-13 18:12:50 +0900

    update

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -6,35 +6,9 @@ class TopController < ApplicationController
   #-------#
   def index
     Tweet.get_tweet( current_user ) unless session[:user_id].blank?
-    print "[ Tweet.count ] : " ; p Tweet.count ;
+    @count = Tweet.count
 
-    @tweets = Tweet.all
-    print "[ @tweets.class.name ] : " ; p @tweets.class.name ;
-    print "[ @tweets.class.name ] : " ; p @tweets.all.class.name ;
-#    print "[ @tweets.class.name ] : " ; p @tweets.length ;
-
-    # @tweet = Tweet.all[0]
-    # print "[ @tweet.class.name ] : " ; p @tweet.class.name ;
-
-    @tweets = Tweet.limit(10).all
-    print "[ @tweets.class.name ] : " ; p @tweets.class.name ;
-
-    @tweets = Tweet.where( id_str: "223489010612441088" )
-    print "[ @tweets.class.name ] : " ; p @tweets.class.name ;
-    @tweets = @tweets.all
-    print "[ @tweets.class.name ] : " ; p @tweets.class.name ;
-
-    @tweets = Tweet.order_by( tweet_at: :desc ).all
-    print "[ @tweets.class.name ] : " ; p @tweets.class.name ;
-
-#    @tweets = (Tweet.count > 0) ? Tweet.all : nil
-#    print "[ Tweet.class.name ] : " ; p Tweet.class.name ;
-    # print "[ @tweets ] : " ; p @tweets ;
-    print "[ @tweets ] : " ; p @tweets ;
-#    @tweets = @tweets.to_a
-#    print "[ @tweets ] : " ; p @tweets ;
-    print "[ @tweets.class.name ] : " ; p @tweets.class.name ;
-#    print "[ nil.to_a ] : " ; p nil.to_a ;
+    @tweets = Tweet.order_by( :tweet_at => :desc ).limit(100).all
   end
 
 end
